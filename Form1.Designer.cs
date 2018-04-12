cec082db4f6662bcdd20c0809061d9fe86d9ecc1
Peter Tettero <peter@udoc.nl>
2018-04-12 16:20:31 +0200

    Added app files

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FileBackupZipper
+{
+    partial class BankFileZipper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bankfile location:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output folder:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please select folder";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Please select folder";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(187, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Backup now!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Starcraft not found";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Last backup updated:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(130, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Never";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 195);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 19);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BankFileZipper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 226);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BankFileZipper";
+            this.Text = "BankFileZipper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace FileBackupZipper
+{
+    public partial class BankFileZipper : Form
+    {
+        const string SC2_PROCESS_NAME = "sc2";
+
+        public BankFileZipper()
+        {
+            InitializeComponent();
+
+            label3.Text = Properties.Settings.Default["BankFilePath"].ToString();
+            label4.Text = Properties.Settings.Default["OutputFolder"].ToString();
+            label7.Text = (Properties.Settings.Default.LastBackupDate != default(DateTime)) ? Properties.Settings.Default.LastBackupDate.ToString("yyyy-MM-dd HH:mm") : "Never";
+
+            pictureBox1.BackColor = System.Drawing.Color.Red;
+            ListenToProgramStartup();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string defaultFolder = "";
+            if (label3.Text != "")
+            {
+                defaultFolder = label3.Text;
+            }
+            
+            string path = SelectFolder(defaultFolder);
+            if (path != "")
+            {
+                label3.Text = path;
+
+                Properties.Settings.Default["BankFilePath"] = path;
+                Properties.Settings.Default.Save();
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string defaultFolder = "";
+            if (label4.Text != "") {
+                defaultFolder = label4.Text;
+            }
+            
+            string path = SelectFolder(defaultFolder);
+            if (path != "") {
+                label4.Text = path;
+                Properties.Settings.Default["OutputFolder"] = path;
+                Properties.Settings.Default.Save();
+            };
+        }
+
+        private string SelectFolder(string defaultFolder)
+        {
+            if (defaultFolder == "")
+            {
+                defaultFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog
+            {
+                InitialDirectory = defaultFolder,
+                IsFolderPicker = true
+            };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+               return dialog.FileName;
+            }
+
+            return "";
+        }
+
+        private void ZipIt(string startPath, string outputPath)
+        {
+            try
+            {
+                ZipFile.CreateFromDirectory(startPath, outputPath, CompressionLevel.Fastest, true);
+
+                Properties.Settings.Default["LastBackupDate"] = DateTime.Now;
+                Properties.Settings.Default.Save();
+
+                label7.Invoke((MethodInvoker)delegate
+                {
+                    label7.Text = Properties.Settings.Default.LastBackupDate.ToString("yyyy-MM-dd HH:mm");
+                });
+            }
+            catch (Exception e) {
+                MessageBox.Show(String.Format(e.Message), "Error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ZipIt(@label3.Text, @label4.Text + "/backup-" + DateTime.Now.ToString("yyyyMMddHHmm") + ".zip");
+        }
+
+        private async void ListenToProgramStartup()
+        {
+            var searching = true;
+
+            while (searching) {
+                await Task.Run(async () => {
+                    searching = !(await IsProcessRunning(SC2_PROCESS_NAME));
+                    await Task.Delay(2000);
+                });
+            }
+
+            ListenToProgramStartup();
+        }
+
+        private void SetProcessFound()
+        {
+            label5.Invoke((MethodInvoker)delegate {
+                label5.Text = "Starcraft found!";
+            });
+
+            pictureBox1.Invoke((MethodInvoker)delegate{
+                pictureBox1.BackColor = System.Drawing.Color.Green;
+            });
+        }
+
+        private void SetProcessLost()
+        {
+            label5.Invoke((MethodInvoker)delegate {
+                label5.Text = "Starcraft not running";
+            });
+
+            pictureBox1.Invoke((MethodInvoker)delegate {
+                pictureBox1.BackColor = System.Drawing.Color.Red;
+            });
+        }
+
+        private async Task<bool> IsProcessRunning(string sProcessName)
+        {
+            Process[] proc = Process.GetProcessesByName(sProcessName);
+            if (proc.Length > 0)
+            {
+                SetProcessFound();
+
+                await Task.Run(() => {
+                    proc[0].WaitForExit();
+                    ZipIt(@label3.Text, @label4.Text + "/backup-"+ DateTime.Now.ToString("yyyyMMddHHmm") + ".zip");
+                });
+
+                return true;
+            }
+
+            SetProcessLost();
+
+            return false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            OpenExplorer(((Label)sender).Text);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            OpenExplorer(((Label)sender).Text);
+        }
+
+        private void OpenExplorer(string path)
+        {
+            if (path == "Empty")
+            {
+                return;
+            }
+
+            Process.Start("explorer.exe", "\"" + path + "\"");
+        }
+    }
+}
